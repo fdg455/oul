cfc6636aa1d97fa8d5ed34161c6686055df90cc1
Ou Bowu <377793729@qq.com>
2016-03-11 23:07:55 +0800

    Support Library更新至23.2.1修复TabLayout的bug，OkHttp添加一个打印返回json的器截器

--- app/src/main/java/com/oushangfeng/ounews/bean/WeatherInfo.java
+++ app/src/main/java/com/oushangfeng/ounews/bean/WeatherInfo.java
@@ -17,24 +17,10 @@ import java.util.List;
 public class WeatherInfo {
 
 
-    /**
-     * desc : OK
-     * status : 1000
-     * data : {"wendu":"-5","ganmao":"澶╁喎椋庡ぇ锛屾槗鍙戠敓鎰熷啋锛岃娉ㄦ剰閫傚綋澧炲姞琛ｆ湇锛屽姞寮鸿嚜鎴戦槻鎶ら伩鍏嶆劅鍐掋\u20ac�","forecast":[{"fengxiang":"鍖楅","fengli":"5-6绾�","high":"楂樻俯 4鈩�","type":"闃�","low":"浣庢俯 -5鈩�","date":"14鏃ユ槦鏈熷ぉ"},{"fengxiang":"鏃犳寔缁鍚�","fengli":"4-5绾�","high":"楂樻俯 1鈩�","type":"鏅�","low":"浣庢俯 -5鈩�","date":"15鏃ユ槦鏈熶竴"},{"fengxiang":"鏃犳寔缁鍚�","fengli":"寰绾�","high":"楂樻俯 4鈩�","type":"鏅�","low":"浣庢俯 -3鈩�","date":"16鏃ユ槦鏈熶簩"},{"fengxiang":"鏃犳寔缁鍚�","fengli":"寰绾�","high":"楂樻俯 8鈩�","type":"鏅�","low":"浣庢俯 -2鈩�","date":"17鏃ユ槦鏈熶笁"},{"fengxiang":"鏃犳寔缁鍚�","fengli":"寰绾�","high":"楂樻俯 10鈩�","type":"鏅�","low":"浣庢俯 -2鈩�","date":"18鏃ユ槦鏈熷洓"}],"yesterday":{"fl":"5-6绾�","fx":"鍖楅","high":"楂樻俯 4鈩�","type":"闃�","low":"浣庢俯 -6鈩�","date":"13鏃ユ槦鏈熷叚"},"aqi":"35","city":"鍖椾含"}
-     */
-
     @JsonProperty("desc")
     public String desc;
     @JsonProperty("status")
     public int status;
-    /**
-     * wendu : -5
-     * ganmao : 澶╁喎椋庡ぇ锛屾槗鍙戠敓鎰熷啋锛岃娉ㄦ剰閫傚綋澧炲姞琛ｆ湇锛屽姞寮鸿嚜鎴戦槻鎶ら伩鍏嶆劅鍐掋€�
-     * forecast : [{"fengxiang":"鍖楅","fengli":"5-6绾�","high":"楂樻俯 4鈩�","type":"闃�","low":"浣庢俯 -5鈩�","date":"14鏃ユ槦鏈熷ぉ"},{"fengxiang":"鏃犳寔缁鍚�","fengli":"4-5绾�","high":"楂樻俯 1鈩�","type":"鏅�","low":"浣庢俯 -5鈩�","date":"15鏃ユ槦鏈熶竴"},{"fengxiang":"鏃犳寔缁鍚�","fengli":"寰绾�","high":"楂樻俯 4鈩�","type":"鏅�","low":"浣庢俯 -3鈩�","date":"16鏃ユ槦鏈熶簩"},{"fengxiang":"鏃犳寔缁鍚�","fengli":"寰绾�","high":"楂樻俯 8鈩�","type":"鏅�","low":"浣庢俯 -2鈩�","date":"17鏃ユ槦鏈熶笁"},{"fengxiang":"鏃犳寔缁鍚�","fengli":"寰绾�","high":"楂樻俯 10鈩�","type":"鏅�","low":"浣庢俯 -2鈩�","date":"18鏃ユ槦鏈熷洓"}]
-     * yesterday : {"fl":"5-6绾�","fx":"鍖楅","high":"楂樻俯 4鈩�","type":"闃�","low":"浣庢俯 -6鈩�","date":"13鏃ユ槦鏈熷叚"}
-     * aqi : 35
-     * city : 鍖椾含
-     */
 
     @JsonProperty("data")
     public DataEntity data;
@@ -58,14 +44,6 @@ public class WeatherInfo {
         public String wendu;
         @JsonProperty("ganmao")
         public String ganmao;
-        /**
-         * fl : 5-6绾�
-         * fx : 鍖楅
-         * high : 楂樻俯 4鈩�
-         * type : 闃�
-         * low : 浣庢俯 -6鈩�
-         * date : 13鏃ユ槦鏈熷叚
-         */
 
         @JsonProperty("yesterday")
         public YesterdayEntity yesterday;
@@ -73,14 +51,6 @@ public class WeatherInfo {
         public String aqi;
         @JsonProperty("city")
         public String city;
-        /**
-         * fengxiang : 鍖楅
-         * fengli : 5-6绾�
-         * high : 楂樻俯 4鈩�
-         * type : 闃�
-         * low : 浣庢俯 -5鈩�
-         * date : 14鏃ユ槦鏈熷ぉ
-         */
 
         @JsonProperty("forecast")
         public List<ForecastEntity> forecast;
